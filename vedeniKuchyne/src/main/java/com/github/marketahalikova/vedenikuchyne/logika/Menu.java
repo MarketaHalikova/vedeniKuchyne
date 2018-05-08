ea98e1359d474daadca6a04896c14e94d0ce9f52
martinweisser <martinweisser@10.0.0.9>
2018-05-08 17:21:40 +0200

    Vytvoření getterů a setterů pro třídu Menu

--- vedeniKuchyne/src/main/java/com/github/marketahalikova/vedenikuchyne/logika/Menu.java
+++ vedeniKuchyne/src/main/java/com/github/marketahalikova/vedenikuchyne/logika/Menu.java
@@ -1,9 +1,32 @@
 package com.github.marketahalikova.vedenikuchyne.logika;
 
+import java.util.List;
+
+/**
+ * 
+ * @author Markéta Halíková, Johanna Švugorevá, Martin Weisser
+ *
+ */
 public class Menu {
 
-	private char datum;
+	private String datum;
+	
+	private List<Recept> SeznamReceptu;
 
-	private Recept seznamReceptu;
+	public String getDatum() {
+		return datum;
+	}
+
+	public void setDatum(String datum) {
+		this.datum = datum;
+	}
+
+	public List<Recept> getSeznamReceptu() {
+		return SeznamReceptu;
+	}
+
+	public void setSeznamReceptu(List<Recept> seznamReceptu) {
+		SeznamReceptu = seznamReceptu;
+	}
 
 }
