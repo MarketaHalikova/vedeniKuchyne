ceb035c95094c504297db08def5cb1a775fdd1d4
MarketaHalikova <halikova.marketa@seznam.cz>
2018-05-08 17:16:08 +0200

    Implementace tříd NakupniSeznam a Sklad

--- vedeniKuchyne/src/main/java/com/github/marketahalikova/vedenikuchyne/logika/NakupniSeznam.java
+++ vedeniKuchyne/src/main/java/com/github/marketahalikova/vedenikuchyne/logika/NakupniSeznam.java
@@ -1,7 +1,23 @@
 package com.github.marketahalikova.vedenikuchyne.logika;
 
+import java.util.List;
+
+
+/**
+ * 
+ * @author Johanna Švugerová, Markéta Halíková, Martin Weisser
+ *
+ */
 public class NakupniSeznam {
 
 	private Surovina seznamSuroviny;
 
+	
+	private List<Surovina> seznamSurovin;
+
+
+	public List<Surovina> getSeznamSurovin() {
+	return seznamSurovin;
+}
+	
 }
--- vedeniKuchyne/src/main/java/com/github/marketahalikova/vedenikuchyne/logika/Sklad.java
+++ vedeniKuchyne/src/main/java/com/github/marketahalikova/vedenikuchyne/logika/Sklad.java
@@ -1,7 +1,18 @@
 package com.github.marketahalikova.vedenikuchyne.logika;
 
+import java.util.List;
+
+/**
+ * 
+ * @author Johanna Švugerová, Markéta Halíková, Martin Weisser
+ *
+ */
 public class Sklad {
 
-	private Surovina seznamSurovin;
+	private List<Surovina> seznamSurovin;
 
+
+	public List<Surovina> getSeznamSurovin() {
+	return seznamSurovin;
+}
 }
