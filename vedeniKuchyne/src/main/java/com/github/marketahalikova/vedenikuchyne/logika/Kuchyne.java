f05393760b2302ec515c7f418dd407caf406a290
MarketaHalikova <halikova.marketa@seznam.cz>
2018-05-10 14:04:01 +0200

    implementace třídy a konstruktoru

---
+++ vedeniKuchyne/src/main/java/com/github/marketahalikova/vedenikuchyne/logika/Kuchyne.java
@@ -0,0 +1,30 @@
+package com.github.marketahalikova.vedenikuchyne.logika;
+
+import java.util.Observable;
+
+
+
+/**
+ * Class Kuchyne 
+ * 
+ * 
+ *
+ * @author     
+ * @version    
+ */
+public class Kuchyne extends Observable{
+
+	public Menu menu;
+	public NakupniSeznam nakupniSeznam;
+	public Sklad sklad;
+	
+	
+	public Kuchyne() {
+        
+        this.menu = new Menu();
+        this.nakupniSeznam = new NakupniSeznam();
+        this.sklad = new Sklad();
+    }
+	
+	
+}
