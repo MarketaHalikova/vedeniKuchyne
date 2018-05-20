70f45e4c36a4e31e3376a18d5ec64bc3cbde9ff7
martinweisser <martinweisser@192.168.0.12>
2018-05-20 13:00:08 +0200

    Tooltip a otevření okna nápověda k jednotkám (obrazvoka UpravitRecept)

--- vedeniKuchyne/src/main/java/com/github/marketahalikova/vedenikuchyne/ui/UpravitReceptController.java
+++ vedeniKuchyne/src/main/java/com/github/marketahalikova/vedenikuchyne/ui/UpravitReceptController.java
@@ -1,5 +1,30 @@
 package com.github.marketahalikova.vedenikuchyne.ui;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 public class UpravitReceptController {
+	
+	/**
+	 * Metoda otevření okna nápověda jednotek
+	 * @throws IOException
+	 */
+	@FXML public void otevritNapoveda() throws IOException {
+		
+		//provizorní řešení
+		
+		StackPane newScene = new StackPane();
+		Scene scene = new Scene(newScene, 300, 100);
+        Stage provizorni = new Stage();
+        provizorni.setScene(scene);
+        provizorni.setAlwaysOnTop(true);
+        provizorni.centerOnScreen();
+        provizorni.show();
+        
+    }
 
 }
