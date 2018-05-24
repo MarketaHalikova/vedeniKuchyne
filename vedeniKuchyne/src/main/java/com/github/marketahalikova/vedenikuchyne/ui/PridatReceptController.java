0539c91091f615ceb3a1bedf05b2e3891cd6bfe4
martinweisser <martinweisser@192.168.0.12>
2018-05-24 22:20:52 +0200

    Přidání comboBoxů s enumy v obrazovce Přidat recept

--- vedeniKuchyne/src/main/java/com/github/marketahalikova/vedenikuchyne/ui/PridatReceptController.java
+++ vedeniKuchyne/src/main/java/com/github/marketahalikova/vedenikuchyne/ui/PridatReceptController.java
@@ -1,5 +1,16 @@
 package com.github.marketahalikova.vedenikuchyne.ui;
 
+import com.github.marketahalikova.vedenikuchyne.logika.Surovina.Jednotka;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class PridatReceptController {
+	
+	@FXML
+	private ComboBox<Jednotka> jednotka;
+	@FXML
+	private ComboBox<String> kategorie;
+
 
 }
