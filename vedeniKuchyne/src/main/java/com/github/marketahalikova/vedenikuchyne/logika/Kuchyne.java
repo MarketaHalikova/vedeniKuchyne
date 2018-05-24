9b5fdaabd550f72f92ede6aa29fa13c70a8e8086
MarketaHalikova <halikova.marketa@seznam.cz>
2018-05-24 11:26:09 +0200

    přidání metod pro srovnání surovin se skladem do třídy Kuchyně

--- vedeniKuchyne/src/main/java/com/github/marketahalikova/vedenikuchyne/logika/Kuchyne.java
+++ vedeniKuchyne/src/main/java/com/github/marketahalikova/vedenikuchyne/logika/Kuchyne.java
@@ -36,7 +36,7 @@ public class Kuchyne extends Observable {
 		// tady bude naplneni provizornimi daty
 		
 		Surovina knedlik = new Surovina("knedlík", Jednotka.ks, 3);
-		Surovina marijanka = new Surovina("marijanka", Jednotka.kg, 20);
+		Surovina marijanka = new Surovina("marijanka", Jednotka.kg, 25);
 		Surovina mrkev = new Surovina("mrkev", Jednotka.ks, 3);
 		List<Surovina> seznamSurovinReceptu = new ArrayList<>();
 		seznamSurovinReceptu.add(knedlik);
@@ -55,7 +55,8 @@ public class Kuchyne extends Observable {
 		menu.vlozitRecept(new Recept("Sushi", "Zabij rybu, uvař rýži.", "krm", seznamSurovinReceptu ));
 		menu.vlozitRecept(new Recept("Losos", "Zabij rybu.", "krm", seznamSurovinReceptu ));
 
-		
+		//System.out.println(srovnaniSurovinReceptuSeSkladem(new Recept("Svíčková", "Uvař svíčkovou.", "krm", seznamSurovinReceptu)));
+
 	}
 
 	/**
@@ -93,5 +94,84 @@ public class Kuchyne extends Observable {
 	public SeznamReceptu getAktualniSeznamReceptu() {
 		return seznamReceptu;
 	}
+	
+	
+	/**
+	 * Metoda porovnává setříděn seznam surovin menu se seznamem surovin na skladě
+	 * suroviny, které je potřeba dokoupit (případně množství existujících surovin) dá do nákoupního seznamu
+	 * 
+	 */
+	public void srovnaniSurovinMenuSeSkladem() {
+		
+		nakupniSeznam.setSeznamSurovin(null);
+		List<Surovina> provizorniSeznam = new ArrayList<>();
+
+
+		for (Surovina surovinaMenu : menu.vytvoreniSeznamuSurovinMenu()) {
+			if(sklad.getSeznamSurovinSkladu().contains(surovinaMenu)) {
+				Surovina sur = sklad.getSeznamSurovinSkladu().get(sklad.getSeznamSurovinSkladu().indexOf(surovinaMenu));
+				if(sur.getMnozstvi()<surovinaMenu.getMnozstvi()){
+					provizorniSeznam.add(new Surovina(sur.getNazev(), sur.getJednotka(), round(surovinaMenu.getMnozstvi() - sur.getMnozstvi(),1)));
+				}
+			} else {
+				provizorniSeznam.add(surovinaMenu);
+			}
+		}
+		
+		nakupniSeznam.setSeznamSurovin(provizorniSeznam);
+		
+		//System.out.println("nakupni listek: " + nakupniSeznam.nakupniSeznamToString()); // 3 suroviny
+																	// suroviny a by melo byt jeden kus;
+																	// suroviny f 0.4g;
+																	// suroviny d 4ks
+	}
+
+	/**
+	 * Metoda porovnává seznam surovin receptu se seznamem surovin na skladě
+	 * suroviny, které je potřeba pro výrobu receptu dokoupit (případně množství existujících surovin) 
+	 * vrátí jako seznam stringů
+	 * 
+	 * @param Recept
+	 * @return List<String>
+	 */
+	public List<String> srovnaniSurovinReceptuSeSkladem(Recept recept) {
+		
+		List<Surovina> SurovinyChybejiciNaSklade = new ArrayList<>();
+		List<Surovina> provizorniSeznam = new ArrayList<>();
+
+
+		for (Surovina surovinaMenu : recept.getSeznamSurovinReceptu()) {
+			if(sklad.getSeznamSurovinSkladu().contains(surovinaMenu)) {
+				Surovina sur = sklad.getSeznamSurovinSkladu().get(sklad.getSeznamSurovinSkladu().indexOf(surovinaMenu));
+				if(sur.getMnozstvi()<surovinaMenu.getMnozstvi()){
+					provizorniSeznam.add(new Surovina(sur.getNazev(), sur.getJednotka(), round(surovinaMenu.getMnozstvi() - sur.getMnozstvi(),1)));
+				}
+			} else {
+				provizorniSeznam.add(surovinaMenu);
+			}
+		}
+		
+		SurovinyChybejiciNaSklade.addAll(provizorniSeznam);
+		
+		List<String> SurovinyChybejiToString = new ArrayList<>();
+		for (Surovina surovina : SurovinyChybejiciNaSklade) {
+			SurovinyChybejiToString.add(surovina.getNazev() + " " + surovina.getMnozstvi() + surovina.getJednotka());
+		}
+		
+		return SurovinyChybejiToString;
+	}
+	
+	/**
+	 * Metoda zaokrouhlí double z parametru
+	 * na počet desetiných míst z precision v parametru
+	 * 
+	 * @param double, int
+	 * @return double
+	 */
+	private static double round (double value, int precision) {
+	    int scale = (int) Math.pow(10, precision);
+	    return (double) Math.round(value * scale) / scale;
+	}
+	
 
 }
